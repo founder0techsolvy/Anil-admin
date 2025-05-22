649c53b2b5fa8ceb0ca0db651d626f333d7b79e0
Avinash Mishra <founder.techsolvy@gmail.com>
2025-05-22 09:27:38 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCeQlNoFs9oMFWS-oDvPYOAu86OYe4GfhQ",
+  authDomain: "capturedeep-126bf.firebaseapp.com",
+  databaseURL: "https://capturedeep-126bf-default-rtdb.firebaseio.com",
+  projectId: "capturedeep-126bf",
+  storageBucket: "capturedeep-126bf.appspot.com",
+  messagingSenderId: "527347157327",
+  appId: "1:527347157327:web:a9df4fb9085b49cf47dd3a",
+  measurementId: "G-TRY1CSCXF7"
+};
